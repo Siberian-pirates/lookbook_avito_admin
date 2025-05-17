d6bdcc10da6e4d9f09748421854ed00abd7c5d7c
foreva <mrforeva0007@gmail.com>
2025-05-17 16:54:09 +0700

    Добавил sass

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    implementation: "dart-sass",
+    api: "modern-compiler",
+  },
 };
 
 export default nextConfig;
